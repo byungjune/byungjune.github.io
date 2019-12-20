7dee2d3670c9f9b763eecf7a34763bcb6fd90945
Byungjune YOON <thankyoon@dano.me>
2019-12-20 15:17:29 +0900

    Force rebuild of site

---
+++ static/const/index.js
@@ -0,0 +1,502 @@
+const matrixData = {
+  "0": {
+    leftShoulder: {
+      x: 374,
+      y: 192,
+    },
+    rightShoulder: {
+      x: 464,
+      y: 201,
+    },
+    leftElbow: {
+      x: 341,
+      y: 265,
+    },
+    rightElbow: {
+      x: 478,
+      y: 273,
+    },
+    leftWrist: {
+      x: 336,
+      y: 323,
+    },
+    rightWrist: {
+      x: 487,
+      y: 333,
+    },
+    leftHip: {
+      x: 377,
+      y: 335,
+    },
+    rightHip: {
+      x: 435,
+      y: 339,
+    },
+    leftKnee: {
+      x: 368,
+      y: 434,
+    },
+    rightKnee: {
+      x: 434,
+      y: 442,
+    },
+    leftAnkle: {
+      x: 358,
+      y: 517,
+    },
+    rightAnkle: {
+      x: 429,
+      y: 531,
+    },
+  },
+  "1": {
+    leftShoulder: {
+      x: 354,
+      y: 178,
+    },
+    rightShoulder: {
+      x: 421,
+      y: 172,
+    },
+    leftElbow: {
+      x: 333,
+      y: 100,
+    },
+    rightElbow: {
+      x: 432,
+      y: 88,
+    },
+    leftWrist: {
+      x: 365,
+      y: 25,
+    },
+    rightWrist: {
+      x: 412,
+      y: 19,
+    },
+    leftHip: {
+      x: 360,
+      y: 330,
+    },
+    rightHip: {
+      x: 405,
+      y: 331,
+    },
+    leftKnee: {
+      x: 360,
+      y: 433,
+    },
+    rightKnee: {
+      x: 476,
+      y: 410,
+    },
+    leftAnkle: {
+      x: 384,
+      y: 523,
+    },
+    rightAnkle: {
+      x: 416,
+      y: 416,
+    }
+  },
+  "2": {
+    leftShoulder: {
+      x: 453,
+      y: 202,
+    },
+    rightShoulder: {
+      x: 540,
+      y: 171,
+    },
+    leftElbow: {
+      x: 396,
+      y: 225,
+    },
+    rightElbow: {
+      x: 598,
+      y: 135,
+    },
+    leftWrist: {
+      x: 445,
+      y: 161,
+    },
+    rightWrist: {
+      x: 524,
+      y: 111,
+    },
+    leftHip: {
+      x: 471,
+      y: 341,
+    },
+    rightHip: {
+      x: 529,
+      y: 344,
+    },
+    leftKnee: {
+      x: 376,
+      y: 349,
+    },
+    rightKnee: {
+      x: 506,
+      y: 418,
+    },
+    leftAnkle: {
+      x: 342,
+      y: 415,
+    },
+    rightAnkle: {
+      x: 486,
+      y: 522,
+    },
+  },
+  "3": {
+    leftShoulder: {
+      x: 350,
+      y: 168,
+    },
+    rightShoulder: {
+      x: 448,
+      y: 217,
+    },
+    leftElbow: {
+      x: 296,
+      y: 109,
+    },
+    rightElbow: {
+      x: 487,
+      y: 246,
+    },
+    leftWrist: {
+      x: 371,
+      y: 114,
+    },
+    rightWrist: {
+      x: 468,
+      y: 183,
+    },
+    leftHip: {
+      x: 361,
+      y: 329,
+    },
+    rightHip: {
+      x: 419,
+      y: 321,
+    },
+    leftKnee: {
+      x: 370,
+      y: 419,
+    },
+    rightKnee: {
+      x: 480,
+      y: 341,
+    },
+    leftAnkle: {
+      x: 385,
+      y: 511,
+    },
+    rightAnkle: {
+      x: 540,
+      y: 436,
+    }
+  },
+  "4": {
+    leftShoulder: {
+      x: 323,
+      y: 165,
+    },
+    rightShoulder: {
+      x: 430,
+      y: 165,
+    },
+    leftElbow: {
+      x: 245,
+      y: 180,
+    },
+    rightElbow: {
+      x: 520,
+      y: 180,
+    },
+    leftWrist: {
+      x: 175,
+      y: 170,
+    },
+    rightWrist: {
+      x: 588,
+      y: 170,
+    },
+    leftHip: {
+      x: 358,
+      y: 340,
+    },
+    rightHip: {
+      x: 452,
+      y: 316,
+    },
+    leftKnee: {
+      x: 380,
+      y: 450,
+    },
+    rightKnee: {
+      x: 515,
+      y: 390,
+    },
+    leftAnkle: {
+      x: 405,
+      y: 540,
+    },
+    rightAnkle: {
+      x: 610,
+      y: 455,
+    }
+  },
+  "5": {
+    leftShoulder: {
+      x: 383,
+      y: 165,
+    },
+    rightShoulder: {
+      x: 490,
+      y: 165,
+    },
+    leftElbow: {
+      x: 295,
+      y: 180,
+    },
+    rightElbow: {
+      x: 580,
+      y: 170,
+    },
+    leftWrist: {
+      x: 235,
+      y: 170,
+    },
+    rightWrist: {
+      x: 655,
+      y: 170,
+    },
+    leftHip: {
+      x: 370,
+      y: 310,
+    },
+    rightHip: {
+      x: 470,
+      y: 344,
+    },
+    leftKnee: {
+      x: 310,
+      y: 390,
+    },
+    rightKnee: {
+      x: 430,
+      y: 448,
+    },
+    leftAnkle: {
+      x: 210,
+      y: 450,
+    },
+    rightAnkle: {
+      x: 410,
+      y: 540,
+    }
+  },
+  "6": {
+    leftShoulder: {
+      x: 405,
+      y: 157,
+    },
+    rightShoulder: {
+      x: 462,
+      y: 224,
+    },
+    leftElbow: {
+      x: 424,
+      y: 93,
+    },
+    rightElbow: {
+      x: 480,
+      y: 290,
+    },
+    leftWrist: {
+      x: 510,
+      y: 75,
+    },
+    rightWrist: {
+      x: 467,
+      y: 309,
+    },
+    leftHip: {
+      x: 355,
+      y: 334,
+    },
+    rightHip: {
+      x: 402,
+      y: 342,
+    },
+    leftKnee: {
+      x: 335,
+      y: 432,
+    },
+    rightKnee: {
+      x: 403,
+      y: 431,
+    },
+    leftAnkle: {
+      x: 314,
+      y: 511,
+    },
+    rightAnkle: {
+      x: 418,
+      y: 515,
+    }
+  },
+  "7": {
+    leftShoulder: {
+      x: 357,
+      y: 236,
+    },
+    rightShoulder: {
+      x: 432,
+      y: 192,
+    },
+    leftElbow: {
+      x: 359,
+      y: 290,
+    },
+    rightElbow: {
+      x: 422,
+      y: 124,
+    },
+    leftWrist: {
+      x: 372,
+      y: 322,
+    },
+    rightWrist: {
+      x: 374,
+      y: 88,
+    },
+    leftHip: {
+      x: 436,
+      y: 347,
+    },
+    rightHip: {
+      x: 482,
+      y: 319,
+    },
+    leftKnee: {
+      x: 408,
+      y: 424,
+    },
+    rightKnee: {
+      x: 485,
+      y: 426,
+    },
+    leftAnkle: {
+      x: 398,
+      y: 487,
+    },
+    rightAnkle: {
+      x: 504,
+      y: 512,
+    }
+  },
+  "8": {
+    leftShoulder: {
+      x: 375,
+      y: 178,
+    },
+    rightShoulder: {
+      x: 447,
+      y: 183,
+    },
+    leftElbow: {
+      x: 346,
+      y: 147,
+    },
+    rightElbow: {
+      x: 482,
+      y: 141,
+    },
+    leftWrist: {
+      x: 379,
+      y: 47,
+    },
+    rightWrist: {
+      x: 477,
+      y: 47,
+    },
+    leftHip: {
+      x: 374,
+      y: 332,
+    },
+    rightHip: {
+      x: 435,
+      y: 334,
+    },
+    leftKnee: {
+      x: 374,
+      y: 434,
+    },
+    rightKnee: {
+      x: 420,
+      y: 435,
+    },
+    leftAnkle: {
+      x: 373,
+      y: 516,
+    },
+    rightAnkle: {
+      x: 413,
+      y: 523,
+    }
+  },
+  "9": {
+    leftShoulder: {
+      x: 360,
+      y: 184,
+    },
+    rightShoulder: {
+      x: 465,
+      y: 190,
+    },
+    leftElbow: {
+      x: 303,
+      y: 188,
+    },
+    rightElbow: {
+      x: 526,
+      y: 200,
+    },
+    leftWrist: {
+      x: 310,
+      y: 122,
+    },
+    rightWrist: {
+      x: 549,
+      y: 153,
+    },
+    leftHip: {
+      x: 373,
+      y: 347,
+    },
+    rightHip: {
+      x: 436,
+      y: 346,
+    },
+    leftKnee: {
+      x: 379,
+      y: 445,
+    },
+    rightKnee: {
+      x: 432,
+      y: 447,
+    },
+    leftAnkle: {
+      x: 376,
+      y: 524,
+    },
+    rightAnkle: {
+      x: 427,
+      y: 525,
+    },
+  }
+}
